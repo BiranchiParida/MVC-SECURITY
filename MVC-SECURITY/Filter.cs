07de92965cbbe3c71a5cb723e903593b152dad6f
Biranchirparida <biranchi104@hotmail.com>
2018-03-29 11:26:15 +0530

    Added the Custom Authorize role In MVC5

--- MVC-SECURITY/Filter.cs
+++ MVC-SECURITY/Filter.cs
@@ -21,4 +21,37 @@ namespace MVC_SECURITY
             filterContext.Result = result;
         }
     }
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)] // it prevent the use of the Multiple time using
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            string role = Convert.ToString(ConfigurationManager.AppSettings["LocalUser"]);
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            //using (DemoDBEntities db = new DemoDBEntities())
+            //{
+            //    UserManager UM = new UserManager();
+            //    foreach (var roles in userAssignedRoles)
+            //    {
+            //        authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+            //        if (authorize)
+            //            return authorize;
+            //    }
+            //}
+            string s = userAssignedRoles.ToString();
+
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Login/UnAuthorized");
+        }
+    }
+
 }
